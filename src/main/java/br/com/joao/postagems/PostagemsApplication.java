1cc004e276daad6ce0295c10842f2e1ad85ff4ed
JoaoEmanoelConinch <jecconinch@gmail.com>
2021-11-30 20:34:32 -0300

    init

---
+++ src/main/java/br/com/joao/postagems/PostagemsApplication.java
@@ -0,0 +1,13 @@
+package br.com.joao.postagems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostagemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostagemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joao/postagems/PostagemsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.postagems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostagemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
